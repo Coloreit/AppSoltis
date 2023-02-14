b377ed24ae1c24fdd3f1728dc2fd26975da85807
Mariana Soltis <marianabs_15@yahoo.com.ar>
2023-02-14 20:37:43 -0300

    clase3

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Texto' style={styles.input} />
+        <Button title='agregar'/>
+      </View>
+      <View style={styles.itemContainer}>
+        <Text style={styles.item}>Item 1</Text>
+        <Text style={styles.item}>Item 2</Text>
+        <Text style={styles.item}>Item 3</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 30,
+  },
+  inputContainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems:'center'
+  },
+  input: {
+    width: 200,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1
+  },
+  itemContainer: {
+    marginTop: 30,
+  },
+  item: {
+    padding: 10,
+    margin: 10,
+    textAlign: 'center',
+    color: 'steelblue',
+    borderRadius: 20,
+    backgroundColor: 'powderblue',
   },
 });
